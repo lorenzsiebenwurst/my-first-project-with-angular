704800e2bf1c76305db436fedff0be194316455c
lorenzSiebenwurst <lo@ohn-line.de>
2022-07-13 13:58:37 +0200

    bulk

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,12 +2,57 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: ` <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+  username = '';
+  allowResetUsername = false;
+  passwordIsHidden = false;
+  clickOnDetailsBtnArray = [];
+  numberOfClicksOnDetailsBtn = 0;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onUsernameInput() {
+    if (this.username !== '') {
+      this.allowResetUsername = true;
+    } else {
+      this.allowResetUsername = false;
+    }
+  }
+
+  onResetUsername() {
+    this.username = '';
+    this.allowResetUsername = false;
+  }
+
+  onDisplayDetails(event) {
+    this.numberOfClicksOnDetailsBtn++;
+    this.passwordIsHidden = !this.passwordIsHidden;
+
+    this.clickOnDetailsBtnArray.push(this.numberOfClicksOnDetailsBtn);
+  }
 }
